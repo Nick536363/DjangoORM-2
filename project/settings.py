9fd6e8c803da32c4dcc6e03e60bc1daac0310c8a
Nick536363 <3383371nik@gmail.com>
2025-07-24 10:58:23 +0200

    Добавил настройки для подключения к БД

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
+
+load_dotenv(find_dotenv())
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': 'checkpoint.devman.org',
+        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
     }
 }
 
