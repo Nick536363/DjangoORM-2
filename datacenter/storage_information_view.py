ddee365f40f35d212d978bc4480cd951737624f4
Nick536363 <3383371nik@gmail.com>
2025-08-04 23:19:40 +0200

    Добавил фильтрацию незакрытых визитов

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,20 +6,27 @@ from datetime import datetime
 from datacenter.models import Visit
 
 
+def get_duration(visit: Visit):
+    time_now =  datetime.now()
+    datetime_enter = str(localtime(visit.entered_at)).split()
+    date_enter = datetime_enter[0].split("-")
+    time_enter = datetime_enter[1].split("+")[0].split(":")
+    datetime_enter = datetime(int(date_enter[0]), int(date_enter[1]), int(date_enter[2]), int(time_enter[0]), int(time_enter[1]), int(time_enter[2]))
+    return (time_now - datetime_enter).total_seconds()
+
+
+def format_duration(total_seconds: int):
+    hours_passed = int(total_seconds // 3600)
+    minutes_passed = int((total_seconds % 3600) / 60)
+    seconds_passed = int((total_seconds % 3600) % 60)
+    return f"{hours_passed}:{minutes_passed}:{seconds_passed}"
+
+
 def storage_information_view(request):
     # Программируем здесь
-    time_now =  datetime.now()
-    for visit in Visit.objects.all():
-        datetime_enter = str(localtime(visit.entered_at)).split()
-        date_enter = datetime_enter[0].split("-")
-        time_enter = datetime_enter[1].split("+")[0].split(":")
-        datetime_enter = datetime(int(date_enter[0]), int(date_enter[1]), int(date_enter[2]), int(time_enter[0]), int(time_enter[1]), int(time_enter[2]))
-        time_delta = (time_now - datetime_enter).total_seconds()
-        hours_passed = int(time_delta // 3600)
-        minutes_passed = int((time_delta % 3600) / 60)
-        seconds_passed = int((time_delta % 3600) % 60)
+    for visit in Visit.objects.filter(leaved_at=None):
         print("\n\n",visit.passcard.owner_name)
-        print("Зашел в хранилище:\n",localtime(visit.entered_at),"\nНаходиться в хранилище:\n",f"{hours_passed}:{minutes_passed}:{seconds_passed}")
+        print("Зашел в хранилище:\n",localtime(visit.entered_at),"\nНаходиться в хранилище:\n", format_duration(get_duration(visit)))
 
     non_closed_visits = [
         {
